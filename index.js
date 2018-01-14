cee5819d1d35d3f7683fb22ec92692108b4fd0af
coldplayer415@yahoo.com Prewitt <coldplayer415@yahoo.com>
2018-01-14 00:10:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ else
   {console.log("Expected the name to be Susan")
     
   }
+if (height === "70")
+    
+  { console.log("The height is correct")
+  
+  }
+else
+   {
+     console.log("Expected the height to be 70")
+   }
 module.exports = { name, height
 }
 
